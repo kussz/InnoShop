da06e8c79195bd3d3b65fae09f0302bd7b64bd7d
kussz <deusvultal4@gmail.com>
2024-10-15 09:31:16 +0300

    Немного изменена структура FormHandler, изменен вывод кэшированных записей

--- InnoShop.ProdWebAPI/Program.cs
+++ InnoShop.ProdWebAPI/Program.cs
@@ -10,6 +10,7 @@ using InnoShop.Infrastructure;
 using Microsoft.Extensions.Caching.Memory;
 using System.Collections;
 using System.Text;
+using Microsoft.AspNetCore.Http;
 
 namespace InnoShop.ProdWebAPI
 {
@@ -54,41 +55,44 @@ namespace InnoShop.ProdWebAPI
                         response += "<tr><td>" + prod.Id +"</td><td>"+prod.Name + "</td><td>" +prod.Description + "</td></tr>";
                     await context.Response.WriteAsync(response + "</table></body></html>");
             });
-            app.Map("/seatchform1", context =>
+            app.Map("/searchform1",async( context) =>
             {
-                app.Use(async (context, next) =>
-                {
                     string? locality = "";
                     if (context.Request.Cookies.ContainsKey("locality"))
                     {
                         locality = context.Request.Cookies["locality"];
                     }
-                    var service = context.RequestServices.GetRequiredService<IServiceManager>();
-                    next.Invoke();
-
-                });
+                    int? prodTypeId = 1;
+                    if (context.Request.Cookies.ContainsKey("prodTypeId"))
+                    {
+                        prodTypeId = Int32.Parse(context.Request.Cookies["prodTypeId"]);
+                    }
+                    context.Items["locality"] = locality;
+                    context.Items["prodTypeId"] = prodTypeId;
+                    await FormHandler(context);
             });
-            app.Map("/seatchform2", context =>
+            app.Map("/searchform2", async( context )=>
             {
-                app.Use(async (context, next) =>
-                {
                     string? locality = "";
                     if (context.Session.Keys.Contains("locality"))
                     {
                         locality = context.Session.GetString("locality");
                     }
-                    var service = context.RequestServices.GetRequiredService<IServiceManager>();
-                    next.Invoke();
+                    int? prodTypeId = 1;
+                    if (context.Session.Keys.Contains("prodTypeId"))
+                    {
+                        prodTypeId = context.Session.GetInt32("prodTypeId");
+                    }
+                    context.Items["locality"] = locality;
+                    context.Items["prodTypeId"] = prodTypeId;
+                    await FormHandler(context);
 
-                });
             });
             app.Map("/products/{id}", (int id, IServiceManager service) =>
             {
                 var product = service.ProductService.GetProduct(id);
                 return $"Id: {product.Id}\nName: {product.Name}\nUser: {product.User.Login}\nCost: {product.Cost}";
             });
-            app.Map("/searchform1", FormHandlerCookies);
-            app.Map("/searchform2", FormHandlerSession);
             app.MapGet("/localities", async (context) =>
             {
                 string locality = "";
@@ -113,78 +117,21 @@ namespace InnoShop.ProdWebAPI
             });
             app.Run();
         }
-        private static void FormHandlerCookies(IApplicationBuilder app)
+        private async static Task FormHandler(HttpContext context)
         {
-            
-            app.Run(async context =>
-            {
-                string locality = "";
-                int? prodTypeId = 1;
-                if (context.Request.Cookies.ContainsKey("locality"))
-                {
-                    locality = context.Request.Cookies["locality"];
-                }
-                else
-                {
-                    locality = "Гомель";
-                    context.Response.Cookies.Append("locality", locality);
-                }
-                if (context.Request.Cookies.ContainsKey("prodTypeId"))
-                {
-                    prodTypeId = Int32.Parse(context.Request.Cookies["prodTypeId"]);
-                }
-                else
-                {
-                    prodTypeId = 1;
-                    context.Response.Cookies.Append("prodTypeId", prodTypeId.ToString());
-                }
+            string locality = context.Items["locality"].ToString();
+            int? prodTypeId = Int32.Parse(context.Items["prodTypeId"].ToString());
+            context.Items.Remove("locality");
+            context.Items.Remove("prodTypeId");
+            var service = context.RequestServices.GetRequiredService<IServiceManager>();
+            string response = "<html><meta charset=\"utf-8\" /><body><form action = 'http://localhost:5036/localities'/ >" +
+            "Locality: <input type = 'text' name = 'locality' value = " + locality + ">" +
+            $"<br>Product type: <select name='prodType'";
+            foreach (var type in service.ProdTypeService.GetAllProdTypes())
+                response += $"<option {(type.Id==prodTypeId?"selected":"")} value='{type.Id}'>{type.Name}</option>";
+            response+="</select><br><br><input type = 'submit' value = 'Сохранить +куки' ></form></body></html>";
 
-                var service = context.RequestServices.GetRequiredService<IServiceManager>();
-                string response = "<html><meta charset=\"utf-8\" /><body><form action = 'http://localhost:5036/localities'/ >" +
-                "Locality: <input type = 'text' name = 'locality' value = " + locality + ">" +
-                $"<br>Product type: <select name='prodType'";
-                foreach (var type in service.ProdTypeService.GetAllProdTypes())
-                    response += $"<option {(type.Id==prodTypeId?"selected":"")} value='{type.Id}'>{type.Name}</option>";
-                response+="</select><br><br><input type = 'submit' value = 'Сохранить +куки' ></form></body></html>";
-
-                await context.Response.WriteAsync(response);
-            });
-        }
-        private static void FormHandlerSession(IApplicationBuilder app)
-        {
-
-            app.Run(async context =>
-            {
-                string? locality = "";
-                int? prodTypeId = 1;
-                if (context.Session.Keys.Contains("locality"))
-                {
-                    locality = context.Session.GetString("locality");
-                }
-                else
-                {
-                    locality = "Гродно";
-                    context.Session.SetString("locality", locality);
-                }
-                if (context.Session.Keys.Contains("prodTypeId"))
-                {
-                    prodTypeId = context.Session.GetInt32("prodTypeId");
-                }
-                else
-                {
-                    context.Session.SetInt32("prodTypeId", (int)prodTypeId);
-                }
-
-                var service = context.RequestServices.GetRequiredService<IServiceManager>();
-                string response = "<html><meta charset=\"utf-8\" /><body><form action = 'http://localhost:5036/localities'/ >" +
-                "Locality: <input type = 'text' name = 'locality' value = " + locality + ">" +
-                "<br>Product type: <select name='prodType'";
-                foreach (var type in service.ProdTypeService.GetAllProdTypes())
-                    response += $"<option {(type.Id == prodTypeId ? "selected" : "")} value='{type.Id}'>{type.Name}</option>";
-                response += "</select><br><br><input type = 'submit' value = 'Сохранить +сессия' ></form></body></html>";
-
-                await context.Response.WriteAsync(response);
-            });
+            await context.Response.WriteAsync(response);
         }
 
     }
